15c8edbaf3f8e40772b795df3c62450b55c527b8
Sean Miller <miller.sean@byu.edu>
2017-04-12 20:36:16 -0600

    finished for the night... almost done with topology changes maybe?

--- broadcast.py
+++ broadcast.py
@@ -18,18 +18,18 @@ class BroadcastApp(object):
         self.distance_vectors[node.hostname] = 0
         #when it begins, we will set timer to timeout
         self.timer = Sim.scheduler.add(delay=self.timeout, event='broadcast', handler=self.broadcast)
+        print(self.node.hostname + ': ' + str(self.distance_vectors))
 
     def receive_packet(self, packet):
         #print(Sim.scheduler.current_time(), self.node.hostname, packet.ident)
-        if(packet.protocol == 'broadcast'):
+        if(packet.protocol == 'dvrouting'):
             self.handle_distance_vector(packet)
 
     def broadcast(self, event):
         self.timer = Sim.scheduler.add(delay=self.timeout, event='broadcast', handler=self.broadcast)
         p = Packet(
-            source_address=self.node.get_address('n1'),
             destination_address=0,
-            ttl=1, protocol='broadcast', body=self.distance_vectors)
+            ttl=1, protocol='dvrouting', body=self.distance_vectors)
         Sim.scheduler.add(delay=0, event=p, handler=self.node.send_packet)
 
     def handle_distance_vector(self, packet):
@@ -37,13 +37,14 @@ class BroadcastApp(object):
     	#if yes: check through and see if anything is at least 2 less than what I have
     	#    if any of them are less than 2 of what I have, that means the path is shorter
     	#else: add everything to my list +1
-        print(packet.body)
         vector_list = packet.body
         for k, v in vector_list.items():
             if k in self.distance_vectors and int(v) <= (int(self.distance_vectors[k])-2):
             	self.distance_vectors[k] = int(v)+1
+            	print(self.node.hostname + ': ' + str(self.distance_vectors))
             elif k not in self.distance_vectors:
             	self.distance_vectors[k] = int(v)+1
+            	print(self.node.hostname + ': ' + str(self.distance_vectors))
         if not self.timer:
             self.timer = Sim.scheduler.add(delay=self.timeout, event='broadcast', handler=self.broadcast)
 
@@ -64,15 +65,15 @@ def main():
 
     # setup broadcast application
     b1 = BroadcastApp(n1)
-    n1.add_protocol(protocol="broadcast", handler=b1)
+    n1.add_protocol(protocol="dvrouting", handler=b1)
     b2 = BroadcastApp(n2)
-    n2.add_protocol(protocol="broadcast", handler=b2)
+    n2.add_protocol(protocol="dvrouting", handler=b2)
     b3 = BroadcastApp(n3)
-    n3.add_protocol(protocol="broadcast", handler=b3)
+    n3.add_protocol(protocol="dvrouting", handler=b3)
     b4 = BroadcastApp(n4)
-    n4.add_protocol(protocol="broadcast", handler=b4)
+    n4.add_protocol(protocol="dvrouting", handler=b4)
     b5 = BroadcastApp(n5)
-    n5.add_protocol(protocol="broadcast", handler=b5)
+    n5.add_protocol(protocol="dvrouting", handler=b5)
 
 
 
